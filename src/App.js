78c3fa95744e97510349d69f89848535f7c3731d
Tamil Selvi <tamilselvi072001@gmail.com>
2023-07-22 18:25:03 +0530

    Basic Login structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import React from "react";
+
+const Login = React.lazy(() => import("./LoginPage/Login"));
+const Signup = React.lazy(() => import("./LoginPage/Signup"));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route exact path="/" name="Login" element={<Login />} />
+          <Route path="/signup" name="Signup" element={<Signup />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/LoginPage/Login.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { CFormLabel, CFormInput, CRow, CCol, CButton } from "@coreui/react";
+import "@coreui/coreui/dist/css/coreui.min.css";
+import "./loginpage.scss";
+import Signup from "./Signup";
+
+const Login = () => {
+  return (
+    <div className="login-page">
+      <div className="brand">
+        <div className="topic-name">Expense Tracker</div>
+        <div className="objective-name">KNOW YOUR EXPENSE</div>
+      </div>
+      <div className="login-container">
+        <div className="login-content">
+          <CRow className="mb-3">
+            <CFormLabel
+              htmlFor="staticEmail"
+              className="col-sm-3 col-form-label">
+              Email
+            </CFormLabel>
+            <CCol sm={9}>
+              <CFormInput
+                type="text"
+                placeholder="abc@example.com"
+                aria-label="default input example"
+              />
+            </CCol>
+          </CRow>
+          <CRow className="mb-3">
+            <CFormLabel
+              htmlFor="inputPassword"
+              className="col-sm-3 col-form-label">
+              Password
+            </CFormLabel>
+            <CCol sm={9}>
+              <CFormInput  className="pass" type="password" id="inputPassword" />
+            </CCol>
+          </CRow>
+          <div className="login-submit-button">
+            <CButton color="secondary">Submit</CButton>
+          </div>
+          <div className="login-signup" onClick={() => window.location.href = '/signup'} >Don't have an account? SignUp</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Login;
